f475928363bbeff5b34a9780967d192d8350c6b4
Ramiro Feichubuinm <ra.feichu@gmail.com>
2024-06-30 13:56:20 -0300

    Primeras optimizaciones con heapq y usando sets

--- TP1/Hanoi/main.py
+++ TP1/Hanoi/main.py
@@ -2,9 +2,9 @@ import time
 
 start = time.time()
 
-
 from hanoi_states import StatesHanoi, ProblemHanoi
 from tree_hanoi import NodeHanoi
+import heapq
 
 initial_state = StatesHanoi([5, 4, 3, 2, 1], [], [], max_disks=5)
 goal_state = StatesHanoi([], [], [5, 4, 3, 2, 1], max_disks=5)
@@ -14,8 +14,9 @@ problem = ProblemHanoi(initial=initial_state, goal=goal_state)
 frontier = [NodeHanoi(problem.initial)]
 explored = set()
 
-# good_order = [[5,4,3,2,1],[5,4,3,2],...,[5]]
-good_oder = [list(range(5,5-(i+1), -1)) for i in range(5)]
+# good_order = {(5,4,3,2,1),(5,4,3,2),...,(5)}
+good_order = [tuple(range(5, 5 - (i + 1), -1)) for i in range(5)]
+good_order_set = set(good_order) #Es mas rapido buscar en un set
 
 while len(frontier) != 0:
     node = frontier.pop()
@@ -30,12 +31,12 @@ while len(frontier) != 0:
     actions = problem.actions(node.state)
     for i, next_node in enumerate(node.expand(problem)):
         rod_3 = next_node.state.rods[2]
-        h = -len(rod_3) if rod_3 in good_oder else 0
-        curr_cost = problem.path_cost(h=h, state1=node.state, action=actions[i], state2=next_node.state)
-        priority_queue.append([curr_cost, next_node])
+        h = -len(rod_3) if rod_3 in good_order else 0
+        # curr_cost = problem.path_cost(h=h, state1=node.state, action=actions[i], state2=next_node.state)
+        curr_cost = h + actions[i].cost
+        heapq.heappush(priority_queue, (curr_cost, next_node))
 
-    sorted_nodes_and_prio = sorted(priority_queue, key=lambda x: x[0])
-    sorted_nodes = [x[1] for x in sorted_nodes_and_prio]
+    sorted_nodes = [heapq.heappop(priority_queue)[1] for _ in range(len(priority_queue))]
 
     for next_node in sorted_nodes:
         if next_node.state not in explored:
