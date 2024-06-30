1e157b4609dc6a2b422b92eac6c4e4d2a7c18e4f
jmestanza <jmestanza@itba.edu.ar>
2024-06-30 17:21:29 -0300

    now priority queue is being used correctly

--- TP1/Hanoi/main2.py
+++ TP1/Hanoi/main2.py
@@ -1,9 +1,6 @@
 import time
-
-from Hanoi.aima import PriorityQueue
-
 start = time.time()
-
+from Hanoi.aima import PriorityQueue
 from hanoi_states import StatesHanoi, ProblemHanoi
 from tree_hanoi import NodeHanoi
 
@@ -16,38 +13,32 @@ frontier = [NodeHanoi(problem.initial)]
 explored = set()
 
 # good_order = [[5,4,3,2,1],[5,4,3,2],...,[5]]
-good_oder = [list(range(5, 5 - (i + 1), -1)) for i in range(5)]
+good_order = {tuple(range(5, 5 - (i + 1), -1)) for i in range(5)}
 
 
-def h(node):
-    rod_3 = node.state.rods[2]
-    h_ = -len(rod_3) if rod_3 in good_oder else 0
-    return h_
+def h(new_node):
+    rod_3 = new_node.state.rods[2]
+    return -len(rod_3) if tuple(rod_3) in good_order else 0
 
 
 def f(new_node):
     return new_node.path_cost + h(new_node)
 
-priority_queue = PriorityQueue(order='min', f=f)
-
+pq = PriorityQueue(order='min', f=f)
+pq.append(NodeHanoi(problem.initial))
 
 while len(frontier) != 0:
-    node = frontier.pop()
+    node = pq.pop()
     explored.add(node.state)
 
     if problem.goal_test(node.state):
         last_node = node
         print("Encontramos la solución")
         break
-
-    priority_queue.extend(node.expand(problem))
-
-    while len(priority_queue) != 0:
-        next_node = priority_queue.pop()
+    for next_node in node.expand(problem):
         if next_node.state not in explored:
+            pq.append(next_node)
             frontier.insert(0, next_node)
-
-print(len(explored), "nodos se expandieron y", len(frontier), "nodos quedaron en la frontera")
-
 end = time.time()
+print(len(explored), "nodos se expandieron y", len(frontier), "nodos quedaron en la frontera")
 print(end - start)
