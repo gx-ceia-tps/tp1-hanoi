a72e596ad70bb1144e6f6a59536bd71ea9062fae
jmestanza <jmestanza@itba.edu.ar>
2024-07-07 20:40:06 -0300

    updated ipynb

--- TP1/Hanoi/main2.py
+++ TP1/Hanoi/main2.py
@@ -8,7 +8,6 @@ goal_state = StatesHanoi([], [], [5, 4, 3, 2, 1], max_disks=5)
 
 problem = ProblemHanoi(initial=initial_state, goal=goal_state)
 
-frontier = [NodeHanoi(problem.initial)]
 explored = set()
 
 good_order = {tuple(range(5, 5 - (i + 1), -1)) for i in range(5)}
@@ -26,8 +25,7 @@ def f(new_node):
 pq = PriorityQueue(order='min', f=f)
 pq.append(NodeHanoi(problem.initial))
 nodes_used = []
-# ----------------------------------
-start = time.time()
+
 while len(pq) != 0:
     node = pq.pop()
     explored.add(node.state)
@@ -39,16 +37,11 @@ while len(pq) != 0:
     for next_node in node.expand(problem):
         if next_node.state not in explored:
             pq.append(next_node)
-            frontier.insert(0, next_node)
 
-
-end = time.time()
-# ----------------------------------
 
 print('nodes used: ', len(nodes_used))
 
-print(len(explored), "nodos se expandieron y", len(frontier), "nodos quedaron en la frontera")
-print(end - start)
+# print(len(explored), "nodos se expandieron y", len(frontier), "nodos quedaron en la frontera")
 
 ans = []
 node = last_node
